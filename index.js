c2cd1539603090c162332eb100eafe2701507853
marcgayete00 <marc.gayete@students.salle.url.edu>
2023-04-20 12:18:06 +0200

    Some things

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+const Minimax = require('tic-tac-toe-minimax');
+const { ComputerMove } = Minimax;
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
 let board = Array(9);
+
+const winningCombinations = [
+  //Horizontal
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  //Vertical
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  //Diagonal
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function isGameOver() {
   if (
@@ -19,6 +40,7 @@ function isGameOver() {
     (board[2] === "X" && board[4] === "X" && board[6] === "X")
   ) {
     console.log("You win!");
+    console.log("Reloading board...");
     board.fill(undefined);
   } else if (
     // Horizontal
@@ -34,18 +56,50 @@ function isGameOver() {
     (board[2] === "O" && board[4] === "O" && board[6] === "O")
   ) {
     console.log("You lose!");
+    console.log("Reloading board...");
+    board.fill(undefined);
+  } else{
+    console.log("Draw!");
+    console.log("Reloading board...");
     board.fill(undefined);
   }
 }
 
 function iaPlays() {
-  let pos = Math.floor(Math.random() * 9);
-  if (board[pos] === undefined) {
-    board[pos] = "O";
-  } else {
-    iaPlays();
+  //Comprobar si el tablero esta lleno
+  let flag = true;
+  for(let i = 0; i < board.length; i++){
+    if(board[i] === undefined){
+      flag = false;
+      break;
+    }
   }
-  isGameOver();
+
+  if (flag){
+    isGameOver();
+  } else {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === "X" && board[i+1] === "X" && board[i+2] === undefined) {
+        pos = i+2;
+        break;
+      } else if (board[i] === "X" && board[i+1] === undefined && board[i+2] === "X"){
+        pos = i+1;
+        break;
+      } else if (board[i] === undefined && board[i+1] === "X" && board[i+2] === "X"){
+        pos = i;
+        break;
+      } else {
+        pos = Math.floor(Math.random() * 9);
+        break;
+      }
+    }
+
+    if (board[pos] === undefined) {
+      board[pos] = "O";
+    } else {
+      iaPlays();
+    }
+  }
 }
 
 app.get("/status", (req, res) => {
